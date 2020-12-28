83ff3e5c4d6cd6d22d148c12ff78e091a12e815c
loik champagne <loik1010@live.com>
2020-12-28 16:05:39 -0500

    Commit #1
    Adding material UI,
    Adding 3 components

---
+++ poolnhl/src/component/NavBarLeft.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class NavBarLeft extends Component {
+    state = {};
+    render() {
+        return <h1>Left</h1>;
+    }
+}
+
+export default NavBarLeft;
